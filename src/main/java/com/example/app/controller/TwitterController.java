db41b805c4e38626e0d22008da6013e921b98ab4
Tomasz <Gitrules8>
2017-07-31 20:39:39 +0200

    added tweets searcher

---
+++ src/main/java/com/example/app/controller/TwitterController.java
@@ -0,0 +1,24 @@
+package com.example.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TwitterController {
+
+    @Autowired
+    private Twitter twitter;
+
+    @RequestMapping("/")
+    public String hello(@RequestParam(defaultValue = "spring") String search, Model model) {
+        SearchResults searchResults = twitter.searchOperations().search(search);
+        String text = searchResults.getTweets().get(0).getText();
+        model.addAttribute("message", text);
+        return "resultPage";
+    }
+}
