2a13632c8a47a99226905a2ddd6257150b6984a8
Tomasz <Gitrules8>
2017-08-28 20:53:03 +0200

    added validation

--- src/main/java/com/example/app/profile/ProfileController.java
+++ src/main/java/com/example/app/profile/ProfileController.java
@@ -2,10 +2,12 @@ package com.example.app.profile;
 
 import com.example.app.date.LocalDateFormatter;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.Locale;
 
 @Controller
@@ -17,7 +19,10 @@ public class ProfileController {
     }
 
     @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public String saveData(ProfileFormDto profileFormDto){
+    public String saveData(@Valid ProfileFormDto profileFormDto, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "profile/profilePage";
+        }
         System.out.println("Profil zapisano pomyślnie" + profileFormDto);
         return "redirect:/profile";
     }
--- src/main/java/com/example/app/profile/ProfileFormDto.java
+++ src/main/java/com/example/app/profile/ProfileFormDto.java
@@ -1,15 +1,24 @@
 package com.example.app.profile;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class ProfileFormDto {
+    @Min(value = 2)
     private String twitterHandle;
+    @Email
+    @NotEmpty
     private String email;
+    @NotNull
     private LocalDate birthDay;
+    @NotEmpty
     private List<String> tastes = new ArrayList<>();
 }
