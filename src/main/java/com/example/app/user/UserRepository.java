ee757b781f3bf1c36fa92cab6e19d7f2de75e406
Tomasz <Gitrules8>
2017-10-21 14:15:04 +0200

    added statuses and documentation to REST api

--- src/main/java/com/example/app/user/UserRepository.java
+++ src/main/java/com/example/app/user/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.app.user;
 
+import com.example.app.error.EntityNotFoundException;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -12,21 +13,30 @@ public class UserRepository {
 
     private final Map<String, User> userMap = new ConcurrentHashMap<>();
 
-    public User save(User user){
-        String email = user.getEmail();
+    public User update(String email, User user) throws EntityNotFoundException {
+        if(!exists(email))
+            throw new EntityNotFoundException("User " + email +  " not exists");
         user.setEmail(email);
         return userMap.put(email,user);
     }
 
-    public User save(String email, User user){
-        return userMap.put(email,user);
+    public User save(User user){
+        return userMap.put(user.getEmail(), user);
+    }
+
+    public User findOne(String email) throws EntityNotFoundException {
+        if(!exists(email))
+            throw new EntityNotFoundException("User " +email+  " not exists");
+        return userMap.get(email);
     }
 
     public List<User> findAll() {
         return new ArrayList<>(userMap.values());
     }
 
-    public void delete(String email){
+    public void delete(String email) throws EntityNotFoundException{
+        if(!exists(email))
+            throw new EntityNotFoundException("User " + email +  " not exists");
         userMap.remove(email);
     }
 
